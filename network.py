f240a4b525b9294e152e7146319f5ea13a7b7f80
accassid <accassid@buffalo.edu>
2016-11-05 18:35:31 -0400

    Finished connecting client to server

--- network.py
+++ network.py
@@ -2,17 +2,19 @@ import select
 import socket
 import sys
 import _thread
-from tkinter import Tk, Label, Button
+from tkinter import *
 
 global sv
+
 
 class Network:
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
+        master.title("Card-Vision")
 
         self.label = Label(master, text="Select if you'd like to listen or connect:")
         self.label.pack()
+
 
         self.cli_button = Button(master, text="Client", command=self.cli)
         self.cli_button.pack()
@@ -20,20 +22,83 @@ class Network:
         self.serv_button = Button(master, text="Server", command=self.serv)
         self.serv_button.pack()
 
+        self.ip_entry = Entry(master, width=24)
+        self.port_entry = Entry(master, width=24)
+        self.ip_label = Label(master, text="Peer IP:")
+        self.port_label = Label(master, text="Peer Port:")
+        self.connect_button = Button(master, text="Connect", command=self.con)
+
+        self.port_listen_label = Label(master, text="Listen on port:")
+        self.listen_button = Button(master, text="Listen", command=self.lis)
+
+        self.listening_label = Label(master, text="You are listening for your peer...")
+        self.cancel_button = Button(master, text="Cancel", command=self.canc)
+
+        self.connected_label = Label(master, text="You are connected!")
+
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
+        global m
+        m = master
 
     def greet(self):
         print("Greetings!")
 
     def cli(self):
-        _thread.start_new_thread( client, ('8.37.55.41',12345) )
+        #_thread.start_new_thread( client, (self.ip_entry.get(),int(self.port_entry.get()) ))
 
         print("You are now a client!")
+        self.label.pack_forget()
+        self.cli_button.pack_forget()
+        self.serv_button.pack_forget()
+        self.close_button.pack_forget()
+
+        self.ip_label.pack()
+        self.ip_entry.pack()
+        self.port_label.pack()
+        self.port_entry.pack()
+        self.connect_button.pack()
+        self.close_button.pack()
+
+    def con(self):
+        _thread.start_new_thread( client, (self.ip_entry.get(),int(self.port_entry.get()) ))
 
     def serv(self):
-        _thread.start_new_thread( server, (12345,) )
+        #_thread.start_new_thread( server, (12345,) )
         print("You are now the server!")
+        for widget in self.master.winfo_children():
+            widget.pack_forget()
+
+
+        self.port_listen_label.pack()
+        self.port_entry.pack()
+        self.listen_button.pack()
+        self.close_button.pack()
+
+    def lis(self):
+        for widget in self.master.winfo_children():
+            widget.pack_forget()
+        _thread.start_new_thread( server, (int(self.port_entry.get()),) )
+        self.listening_label.pack()
+        self.cancel_button.pack()
+        self.close_button.pack()
+
+    def canc(self):
+        for widget in self.master.winfo_children():
+            widget.pack_forget()
+
+        self.label.pack()
+        self.cli_button.pack()
+        self.serv_button.pack()
+        self.close_button.pack()
+
+    def connected(self):
+        for widget in self.master.winfo_children():
+            widget.pack_forget()
+        self.connected_label.pack()
+
+
+
 
 def client(ip, port):
     size = 1024
@@ -41,6 +106,7 @@ def client(ip, port):
     sv = socket.socket()
     sv.connect((ip, port))
     print('Connected to server')
+    my_gui.connected()
     while(True):
         sv.recv(size)
 
@@ -51,7 +117,7 @@ def server(port):
     #hostip = '127.0.0.1'
     hostip = socket.gethostbyname(host)
     print('Your ip is: ', hostip)
-    port = 12345                # Reserve a port for your service.
+    print('You are listening on port: ', port)
     sv.bind((hostip, port))        # Bind to the port
     #port = 50000
     #backlog = 5
@@ -70,6 +136,7 @@ def server(port):
                 client, address = sv.accept()
                 input.append(client)
                 print("New connection: ", address)
+                my_gui.connected()
             elif s == root:
                 root.mainloop()
             elif s == sys.stdin:
@@ -90,6 +157,7 @@ def server(port):
 
 
 root = Tk()
+global my_gui
 my_gui = Network(root)
 #_thread.start_new_thread( server, (1234,) )
 #_thread.start_new_thread( root.mainloop, () )
