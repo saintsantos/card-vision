c59f3b81e1316f01bc4f54d0cf64e90aeb42d8d8
saintsantos <saintsantos1341@gmail.com>
2016-11-06 05:40:24 -0500

    Added missing junk

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import parse_html.tappedout as tappedout
+import tappedout
 import sys
 import pprint
-import shape_detection.detect_shapes as detect_shapes
+import detect_shapes
 
-image = "test.png"
+image = "images/test.png"
 url = sys.argv[1]
 
 deck = tappedout.generateDeck(url)
@@ -13,3 +13,4 @@ status = detect_shapes.boardParser(image, deck)
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(deck)
+        
