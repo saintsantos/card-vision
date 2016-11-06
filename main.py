0d497af5727803cef3a25d026046e3166041dae2
teri511 <willmartens91@gmail.com>
2016-11-06 04:18:19 -0500

    more what

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import parse_html.tappedout as tappedout
 import sys
 import pprint
+import shape_detection.detect_shapes as detect_shapes
 
+image = "test.png"
 url = sys.argv[1]
 
 deck = tappedout.generateDeck(url)
+
+status = detect_shapes.boardParser(image, deck)
+
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(deck)
